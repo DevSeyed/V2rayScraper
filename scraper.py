89a56953a7f54f3598e7976fc61386c2da75e08d
Mahdi Taheri <165409903+DevSeyed@users.noreply.github.com>
2024-06-24 11:09:39 +0330

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,14 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import shutil
-import logging
 from datetime import datetime
 import urllib.parse
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
-
-# Configure logging
-logging.basicConfig(filename='v2ray_scraper.log', level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
